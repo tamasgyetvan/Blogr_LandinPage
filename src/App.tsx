5fab81fcfb9ea2c88e61366f060619938c954fd6
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-12-28 10:59:04 +0100

    Create Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { Header } from "./components/Header/Header";
 import "../src/scss/style.scss";
+import { Hero } from "./components/Hero/Hero";
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Hero></Hero>
+      </main>
     </>
   );
 }
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+export const Hero = () => {
+  return (
+    <div className="hero">
+      <h1 className="heroTitle">A modern publishing platform</h1>
+      <p>Grow your audience and build your online brand</p>
+      <div className="buttonContainer">
+        <button className="startButton">Start for Free</button>
+        <button className="learnMoreButton">Learn More</button>
+      </div>
+    </div>
+  );
+};
