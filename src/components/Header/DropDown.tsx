c53cb3b22bda00cec72b036ebc44f783014f95d1
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2024-01-08 11:31:32 +0100

    remove unnecessary container

--- src/components/Header/DropDown.tsx
+++ src/components/Header/DropDown.tsx
@@ -14,20 +14,18 @@ export const DropDown = ({
 }: DropDownProps) => {
   return (
     <div className="dropDown">
-      <div className="buttonWrapper">
-        <button className="dropDownButton" onClick={handleClick}>
-          {content.title}
-          <svg xmlns="http://www.w3.org/2000/svg" width="10" height="7">
-            <path
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              opacity=".75"
-              d="M1 1l4 4 4-4"
-            />
-          </svg>
-        </button>
-      </div>
+      <button className="dropDownButton" onClick={handleClick}>
+        {content.title}
+        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="7">
+          <path
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            opacity=".75"
+            d="M1 1l4 4 4-4"
+          />
+        </svg>
+      </button>
       {content.title === currentActive ? (
         <div className="dropDownMenu">
           {content.menuItems.map((item) => {
