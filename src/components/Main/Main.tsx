49f371a81c91a7ad63a50fe984cb1ec7b36f0e43
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2024-01-09 19:09:56 +0100

    Create SectionOne component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,41 +1,10 @@
+import { SectionOne } from "./SectionOne";
+
 export const Main = () => {
   return (
     <main>
       <h2>Designed for the future</h2>
-      <section>
-        <div className="textContainer">
-          <div className="textBox">
-            <h3>Introducing an extensible editor</h3>
-            <p>
-              Blogr features an exceedingly intuitive interface which lets you
-              focus on one thing: creating content. The editor supports
-              management of multiple blogs and allows easy manipulation of
-              embeds such as images, videos, and Markdown. Extensibility with
-              plugins and themes provide easy ways to add functionality or
-              change the looks of a blog.
-            </p>
-          </div>
-          <div className="textBox">
-            <h3>Robust content management</h3>
-            <p>
-              Flexible content management enables users to easily move through
-              posts. Increase the usability of your blog by adding customized
-              categories, sections, format, or flow. With this functionality,
-              you’re in full control.
-            </p>
-          </div>
-        </div>
-        <img
-          className="svgDesktop"
-          src="images/illustration-editor-desktop.svg"
-          alt=""
-        />
-        <img
-          className="svgMobile"
-          src="images/illustration-editor-mobile.svg"
-          alt=""
-        />
-      </section>
+      <SectionOne />
     </main>
   );
 };
