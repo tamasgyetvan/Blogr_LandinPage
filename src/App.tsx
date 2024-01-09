a267a1165edf370a6415750a36b0d78a727428e7
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2024-01-09 18:57:48 +0100

    Create first section of Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
 import "../src/scss/style.scss";
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
